67abe2bc3d12213d24dffff4abf53ae030dd7bb2
Jérémie Klein <jeremie.klein@octo.com>
2025-06-01 15:44:36 +0200

    feat: add Gardena Return to Dock button to Smart System integration

--- custom_components/gardena_smart_system/button.py
+++ custom_components/gardena_smart_system/button.py
@@ -17,8 +17,9 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     entities = []
     for mower in hass.data[DOMAIN][GARDENA_LOCATION].find_device_by_type("MOWER"):
         entities.append(GardenaStartOverrideButton(mower, config_entry.options))
+        entities.append(GardenaReturnToDockButton(mower))
 
-    _LOGGER.debug("Adding start override button: %s", entities)
+    _LOGGER.debug("Adding mower buttons: %s", entities)
     async_add_entities(entities, True)
 
 
@@ -75,4 +76,58 @@ class GardenaStartOverrideButton(ButtonEntity):
     async def async_press(self) -> None:
         """Handle the button press."""
         duration = self._options.get("mower_duration", 60) * 60  # Convert minutes to seconds
-        await self._device.start_seconds_to_override(duration) 
+        await self._device.start_seconds_to_override(duration)
+
+
+class GardenaReturnToDockButton(ButtonEntity):
+    """Representation of a Gardena Return to Dock button."""
+
+    def __init__(self, mower):
+        """Initialize the Gardena Return to Dock button."""
+        self._device = mower
+        self._name = f"{self._device.name} Return to Dock"
+        self._unique_id = f"{self._device.serial}-return-to-dock"
+
+    async def async_added_to_hass(self):
+        """Subscribe to events."""
+        self._device.add_callback(self.update_callback)
+
+    @property
+    def should_poll(self) -> bool:
+        """No polling needed for a button."""
+        return False
+
+    def update_callback(self, device):
+        """Call update for Home Assistant when the device is updated."""
+        self.schedule_update_ha_state(True)
+
+    @property
+    def name(self):
+        """Return the name of the device."""
+        return self._name
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique ID."""
+        return self._unique_id
+
+    @property
+    def available(self):
+        """Return True if the device is available."""
+        return self._device.state != "UNAVAILABLE"
+
+    @property
+    def device_info(self):
+        """Return device info."""
+        return {
+            "identifiers": {
+                (DOMAIN, self._device.serial)
+            },
+            "name": self._device.name,
+            "manufacturer": "Gardena",
+            "model": self._device.model_type,
+        }
+
+    async def async_press(self) -> None:
+        """Handle the button press."""
+        await self._device.park_until_next_task() 
